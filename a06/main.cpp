01ccb88bc4cc8da398a3d2ed14320227b4662a2d
Nathan Rodman <nathanrodman@gmail.com>
2013-11-13 16:30:44 -0800

    Initial Commit

---
+++ a06/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  a06
+//
+//  Created by Nathan Rodman on 11/13/13.
+//  Copyright (c) 2013 Nathan Rodman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
